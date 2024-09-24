9c807a55077ba538b8a7051c3aa7921d76a1cd03
MVW <158435+LarkRiseMedia@users.noreply.github.com>
2024-09-24 10:54:37 +0100

    finished Dem

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://steady-youtiao-b2b2ad.netlify.app",
+  integrations: [preact()]
+});
